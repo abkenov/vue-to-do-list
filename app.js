9cf60ec6f27c3566f40f22b647fad2771dbd697b
abkenov <abkenov.ablan@gmail.com>
2021-06-09 17:27:01 +0600

    added v-model and watch

--- app.js
+++ app.js
@@ -4,23 +4,29 @@ const App = {
       myPlaceholder: 'Add new task',
       tasks: [],
       newTaskValue: '',
-      total: 0
+      total: 0,
+      maxInputLength: 40,
+      remainingInputLength: 40
     }
   },
   methods: {
-    newTaskChangeHandler(event) {
-      this.newTaskValue = event.target.value
-    },
 
     addTask() {
       if(this.newTaskValue.length) {
         this.tasks.push(this.newTaskValue)
         this.newTaskValue = ''
+        this.remainingInputLength = this.maxInputLength
       }
     },
 
     removeTask(idx) {
       this.tasks.splice(idx, 1)
+    }
+  },
+  
+  watch: {
+    newTaskValue(value) {
+      this.remainingInputLength = this.maxInputLength - value.length
     }
   },
 
